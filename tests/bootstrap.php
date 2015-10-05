745cb62f62644b5d8af66b760c541e1753c680c2
M. Wacker <wackerm@users.noreply.github.com>
2015-10-05 19:10:16 +0200

    Update bootstrap.php

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,18 +1,9 @@
 <?php
 
-require_once 'mink.phar';
-
-class MinkTestCase extends PHPUnit_Framework_TestCase
-{
-  function setUp()
-  {
-    $this->driver = new \Behat\Mink\Driver\GoutteDriver();
-    $this->session = new \Behat\Mink\Session($this->driver);
-    $this->session->start();
-  }
- 
-  function tearDown()
-  {
-    $this->session->stop();
+//Test für phpUnit
+class VerifyTest extends PHPUnit_Framework_TestCase {
+  public function test_falseIfNoAtSign() {
+    $actual = Verify::checkEmail('manuel.kiessling.net');
+    $this->assertFalse($actual);
   }
 }
